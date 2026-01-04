19c671f30a85a7c01123ad28c91746dac755c091
Jinwoo Kim <36787932+Jinsab@users.noreply.github.com>
2026-01-04 16:30:31 +0900

    유니티 가비지 컬렉션 일부 정리

--- Assets/Scripts/LifeCycle.cs
+++ Assets/Scripts/LifeCycle.cs
@@ -6,7 +6,7 @@ using UnityEngine;
  *  
  *  [학습 일자]
  *  파일 생성 일자 : 26.01.04 오후 15:48
- *  마지막 수정 일자 : 26.01.04 오후 15:48
+ *  마지막 수정 일자 : 26.01.04 오후 16:13
  *  
  *  [학습 내용]
  *  1. 유니티의 흐름
@@ -27,10 +27,27 @@ using UnityEngine;
  *         - OnEnable : Awake 이후에 호출
  *    6-1. 비활성화
  *         - OnDisable : OnDestory 직전에 호출
- *  
+ *  2. 가비지 컬렉터 (GC)
+ *    1-1. 가비지 컬렉터의 기능
+ *         - 애플리케이션과 Unity에서 더 이상 사용하지 않는 오브젝트로부터 메모리를 회수하는 기능
+ *         - 자동으로 작동하지만, 호출 될 때 CPU 시간이 상당히 많이 소요됨.
+ *         - 즉, 최적화를 위해선 카비지 컬렉터를 많이 부르지 않도록 하는 것이 좋음.
+ *         - 너무 빠르다면 성능에 악 영향을, 너무 늦다면 수천 개를 처리해야 하므로 시간이 많이 소모 될 것
+ *    2-1. 최적화 기술
+ *         - 오브젝트 풀링 : 발사체를 비활성화하여 필요할 때 활성화하고 소모(파괴) 이후엔 비활성화 하는 기술
+ *         - string 연결
+ *           - ABCDE를 연결할 때 ABCDE를 한 번에 A, AB, ... ABCDE는 5번의 결과
+ *           - 또한 점수 등을 표시 할 땐, 변경될 때만 텍스트가 업데이트 되도록 하면 됨
+ *         - 박싱 (boxing)
+ *           - 값 타입의 변수가 자동으로 레퍼런스 타입으로 전환될 때 발생
+ *         
  *  [학습 출처]
+ *  생명 주기
  *  1. 유니티 메뉴얼 : https://docs.unity3d.com/kr/2023.2/Manual/ExecutionOrder.html
  *  2. 골드메탈님 영상 : https://youtu.be/PyN3JkPTpAI?si=pHBYXB1Q0M4fyprz
+ *  가비지 컬렉터
+ *  1. https://docs.unity3d.com/kr/2023.2/Manual/performance-garbage-collector.html
+ *  2. https://docs.unity3d.com/kr/2023.2/Manual/performance-garbage-collection-best-practices.html
  */
 
 public class LifeCycle : MonoBehaviour

05ceeace7a5754cad990a8b7018a804c46a25c9c
Jinwoo Kim <36787932+Jinsab@users.noreply.github.com>
2026-01-04 15:43:29 +0900

    유니티 C# 프로그래밍 기초 정리

---
+++ Assets/Scripts/Debug_Log.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+/*  
+ *  [학습 제목]
+ *  콘솔창에 메세지 띄워보기
+ *  
+ *  [학습 일자]
+ *  파일 생성 일자 : 26.01.04 오후 01:35
+ *  마지막 수정 일자 : 26.01.04 오후 01:45
+ *  
+ *  [학습 내용]
+ *  Debug.Log("내용");
+ *  
+ *  유니티 콘솔에 메시지를 기록할 수 있다.
+ */  
+
+public class Debug_Log : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello Unity!");
+    }
+}
